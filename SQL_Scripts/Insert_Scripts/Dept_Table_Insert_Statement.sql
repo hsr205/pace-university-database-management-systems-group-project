55eb83802f34a3ab43620a6660fbe9232c3c28a2
Henry Rothenberg <hsr205@aol.com>
2024-02-20 14:30:55 -0500

    Compelte insert statement scripts

---
+++ SQL_Scripts/Insert_Scripts/Dept_Table_Insert_Statement.sql
@@ -0,0 +1,5 @@
+INSERT INTO Depot (depot_id, depot_address, volume)
+VALUES
+      ('d1','New York',9000),
+      ('d2','Syracuse',6000),
+      ('d4','New York',2000);
---
+++ SQL_Scripts/Insert_Scripts/Product_Table_Insert_Statement.sql
@@ -0,0 +1,5 @@
+INSERT INTO Product (product_id, product_name, price)
+VALUES
+      ('p1','tape',2.5),
+      ('p2','tv',250),
+      ('p3','vcr',80);
