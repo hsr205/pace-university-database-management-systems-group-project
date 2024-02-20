b24d00cc8e849cc242a04af311fa101c524c13b8
Henry Rothenberg <hsr205@aol.com>
2024-02-20 16:37:51 -0500

    Complete transaction 4

--- SQL_Scripts/Transaction_Scripts/Transaction_4_Script.sql
+++ SQL_Scripts/Transaction_Scripts/Transaction_4_Script.sql
@@ -5,11 +5,22 @@
 */
 
 
+BEGIN;
 
+	ALTER TABLE stock DROP CONSTRAINT if exists stock_depot_id_fkey;
 
--- NOT COMPLETE!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    UPDATE stock
+    SET depot_id = 'dd1'
+    WHERE depot_id = 'd1';
 
+    UPDATE depot
+    SET depot_id = 'dd1'
+    WHERE depot_id = 'd1';
+	
+	ALTER TABLE stock ADD CONSTRAINT stock_depot_id_fkey FOREIGN KEY (depot_id)
+	REFERENCES depot (depot_id) NOT VALID;
 
+COMMIT;
 
 
 /*
@@ -39,7 +50,6 @@ product_id		depot_id     quantity
 
 */
 
-
 /*
 
 -- NOTE: AFTER to transaction execution
@@ -47,22 +57,23 @@ product_id		depot_id     quantity
 DEPOT table
 ======================================
 depot_id    depot_address    volume
-"d1"	    "New York"	     9000
 "d2"	    "Syracuse"	     6000
 "d4"	    "New York"	     2000
+"dd1"	    "New York"	     9000
 ======================================
 
 STOCK table
 ======================================
 product_id		depot_id     quantity
-"p1"	        "d1"	     1000
+
 "p1"	        "d2"	     -100
 "p1"	        "d4"	     1200
-"p3"	        "d1"	     3000
 "p3"	        "d4"	     2000
 "p2"	        "d4"	     1500
-"p2"	        "d1"	     -400
 "p2"	        "d2"	     2000
+"p1"	        "dd1"	     1000
+"p3"	        "dd1"	     3000
+"p2"	        "dd1"	     -400
 ======================================
 
 */
